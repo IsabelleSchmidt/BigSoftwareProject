5b24bc62e328e3d4c61b1f432180d0763d00d91b
Ann-Cathrin Fabian <anny@AnnCathrinsMBP.fritz.box>
2020-12-03 13:02:01 +0100

    (#54) & (#61) Zwischenstand

--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/user/User.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/user/User.java
@@ -1,19 +1,144 @@
 package de.hsrm.mi.swtpro.pflamoehus.user;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import de.hsrm.mi.swtpro.pflamoehus.user.adress.Adress;
+import de.hsrm.mi.swtpro.pflamoehus.user.paymentmethods.Bankcard;
+import de.hsrm.mi.swtpro.pflamoehus.user.paymentmethods.Creditcard;
+import de.hsrm.mi.swtpro.pflamoehus.validation.User.ValidBirthDay;
+import de.hsrm.mi.swtpro.pflamoehus.validation.User.ValidGender;
+
 
 @Entity
 public class User {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @JsonIgnore
+    private long id;
+
+    @Version
+    @JsonIgnore
+    private long version;
     
-    @NotNull
-    private String name;
+    @NotEmpty
+    @Column(name="EMAIL", unique=true)
+    @Email
+    private String email;
     
+    @NotEmpty
+    @Size(min=3)
+    @JsonProperty(access =  Access.WRITE_ONLY)
+    private String passwort;
+
+    @NotEmpty  
+    private String firstName;
+
+    private String middleName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    @ValidBirthDay
+    private LocalDate birthdate;
+
+    @NotEmpty
+    @ManyToOne
+    private Adress adress;
+
+    @NotEmpty
+    @ValidGender
+    private String gender;
+
+    @Valid
+    @OneToMany(mappedBy = "user")
+    private List<Bankcard> bankcard;
+
+    @Valid
+    @OneToMany(mappedBy = "user")
+    private List<Creditcard> creditcard;
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Adress getAdress() {
+        return this.adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+
+    public LocalDate getBirthdate() {
+        return this.birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public long getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMiddleName() {
+        return this.middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
 }
--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/user/UserRepository.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/user/UserRepository.java
@@ -1,9 +1,20 @@
 package de.hsrm.mi.swtpro.pflamoehus.user;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import de.hsrm.mi.swtpro.pflamoehus.user.adress.Adress;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByName (String name);
+    User findByEmail (String email);
+    User findByFirstName (String firstName);
+    User findByLastName (String lastName);
+
+    List<User> findByAdress (Adress adress);
     
+    
+
+
 }
