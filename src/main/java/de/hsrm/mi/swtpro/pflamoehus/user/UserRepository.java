4bd79da2520cb1693a55ec4573078e3b98f45e27
Ann-Cathrin Fabian <anny@AnnCathrinsMBP.fritz.box>
2020-12-08 16:47:18 +0100

    (#54) & (#61) fuers Testen bereit

--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/user/UserRepository.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/user/UserRepository.java
@@ -1,16 +1,14 @@
 package de.hsrm.mi.swtpro.pflamoehus.user;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import de.hsrm.mi.swtpro.pflamoehus.user.adress.Adress;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail (String email);
     Optional <User> findById (long id);
-    List<User> findByAdress (Adress adress);
+
     
 }
