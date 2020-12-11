e7a9d58fac48faa7093b134765294234d5ce1fad
Svenja G. Schenk <Svenja.Schenk@student.hs-rm.de>
2020-12-11 16:19:03 +0100

    Picture GET Request und Bilderrepo

--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/product/picture/PictureRepository.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/product/picture/PictureRepository.java
@@ -1,5 +1,11 @@
 package de.hsrm.mi.swtpro.pflamoehus.product.picture;
 
-public class PictureRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+    //TODO: soll alle Bilder mit der passenden Artikelnr finden
+    List<Optional<Picture>> findByProduct(long articleNr);
 }
