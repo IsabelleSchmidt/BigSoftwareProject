35d80669be89533549ab69518f2842d2e72d6e04
Ann-Cathrin Fabian <anncathrin.fabian@gmail.com>
2020-12-17 13:36:16 +0100

    PictureService umbenannt

---
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/product/pictureService/PicturesService.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.swtpro.pflamoehus.product.pictureService;
+import java.util.List;
+import de.hsrm.mi.swtpro.pflamoehus.product.Product;
+import de.hsrm.mi.swtpro.pflamoehus.product.picture.Picture;
+
+public interface PicturesService {
+    
+        public List<Picture> findAll();
+        public Picture findPictureWithID(long id);
+        public List<Picture> findPicturesWithProduct(Product product);
+        public List<Picture> findAllWithPath(String path);
+}
+
