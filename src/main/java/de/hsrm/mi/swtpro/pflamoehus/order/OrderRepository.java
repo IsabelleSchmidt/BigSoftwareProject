af3bb92ccf62908ad3ae640e71bb7c04f15843e9
Ann-Cathrin Fabian <anncathrin.fabian@gmail.com>
2021-01-05 16:36:31 +0100

    #151 Bestellung Repository

---
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/order/OrderRepository.java
@@ -0,0 +1,41 @@
+package de.hsrm.mi.swtpro.pflamoehus.order;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import de.hsrm.mi.swtpro.pflamoehus.user.User;
+
+/*
+ * Order-Repository for different operations to apply on the database.
+ * 
+ * @author Ann-Cathrin Fabian
+ * @version 1
+ */
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    /**
+     * Find a order by its id.
+     * 
+     * @param id wanted order
+     * @return order
+     */
+    Optional<Order> findByOrderNR(long id);
+
+    /**
+     * Find a order by its user.
+     * 
+     * @param user wanted order
+     * @return oder
+     */
+    Order findByUserID (User user);
+
+    /**
+     * Sort all orders by their delivery date.
+     * 
+     * @return list of orders
+     */
+    List<Order> findAllByOrderByDeliveryDateAsc();
+    
+}
