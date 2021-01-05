7a85f40c6d06cc5928bf0b1933e96a104d478ee0
Ann-Cathrin Fabian <anncathrin.fabian@gmail.com>
2021-01-05 17:17:44 +0100

    #154 JavaDoc für Bestelldetails

--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/order/OrderDetails.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/order/OrderDetails.java
@@ -12,6 +12,12 @@ import org.springframework.validation.annotation.Validated;
 
 import de.hsrm.mi.swtpro.pflamoehus.product.Product;
 
+/*
+ * OrderDetails-Entity for its database.
+ * 
+ * @author Svenja Schenk
+ * @version 1
+ */
 @Entity
 @Validated
 public class OrderDetails {
@@ -33,42 +39,96 @@ public class OrderDetails {
     @ManyToOne
     private Product articleNR;
 
+    
+    /** 
+     * Orderdetails to string.
+     * 
+     * @return String
+     */
     @Override
     public String toString() {
         return "OrderDetails [articleNR=" + articleNR + ", orderDetailsID=" + orderDetailsID + ", orderID=" + orderID
                 + ", statusID=" + statusID + ", version=" + version + "]";
     }
 
+    
+    /** 
+     * Get orderdetails-id.
+     * 
+     * @return long
+     */
     public long getOrderDetailsID() {
         return orderDetailsID;
     }
 
 
+    
+    /** 
+     * Get version.
+     * 
+     * @return long
+     */
     public long getVersion() {
         return version;
     }
 
 
+    
+    /** 
+     * Get orderid.
+     * 
+     * @return Order
+     */
     public Order getOrderID() {
         return orderID;
     }
 
+    
+    /** 
+     * Set orderid.
+     * 
+     * @param orderID to be set
+     */
     public void setOrderID(Order orderID) {
         this.orderID = orderID;
     }
 
+    
+    /** 
+     * Get statusid.
+     * 
+     * @return Status
+     */
     public Status getStatusID() {
         return statusID;
     }
 
+    
+    /** 
+     * Set statusid.
+     * 
+     * @param statusID to be set
+     */
     public void setStatusID(Status statusID) {
         this.statusID = statusID;
     }
 
+    
+    /** 
+     * Get articlenr.
+     * 
+     * @return Product
+     */
     public Product getArticleNR() {
         return articleNR;
     }
 
+    
+    /** 
+     * Set articlenr.
+     * 
+     * @param articleNR to be set
+     */
     public void setArticleNR(Product articleNR) {
         this.articleNR = articleNR;
     }
