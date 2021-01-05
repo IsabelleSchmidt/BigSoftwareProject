67e83155e6b4dda7b4c93eb606cef668ff073516
Ann-Cathrin Fabian <anncathrin.fabian@gmail.com>
2021-01-05 16:36:45 +0100

    #154 Javadoc

--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/order/Status.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/order/Status.java
@@ -1,6 +1,7 @@
 package de.hsrm.mi.swtpro.pflamoehus.order;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,6 +15,12 @@ import org.springframework.validation.annotation.Validated;
 
 import de.hsrm.mi.swtpro.pflamoehus.validation.order_db.ValidStatus;
 
+/*
+ * Status-Entity for its database.
+ * 
+ * @author Svenja Schenk
+ * @version 1
+ */
 @Entity
 @Validated
 public class Status {
@@ -27,70 +34,142 @@ public class Status {
 
     @ManyToOne 
     @JsonIgnore
-    private HashSet<Order> allOrderNR = new HashSet<>();
+    private Set<Order> allOrderNR = new HashSet<>();
 
     @ManyToOne
     @JsonIgnore
-    private HashSet<OrderDetails> allOrderDetailNR = new HashSet<>();
+    private Set<OrderDetails> allOrderDetailNR = new HashSet<>();
 
     @ValidStatus
     private String statuscode;
 
- 
-
+    
+    /** 
+     * Get statusId.
+     * 
+     * @return long
+     */
     public long getStatusID() {
         return statusID;
     }
 
+    
+    /** 
+     * Add a new order.
+     * 
+     * @param newOrder to be added
+     * @return boolean
+     */
     public boolean addOrder(Order newOrder){
         return allOrderNR.add(newOrder);
     }
 
+    
+    /** 
+     * Remove a order.
+     * 
+     * @param delOrder to be removed
+     * @return boolean
+     */
     public boolean removeOrder(Order delOrder){
         return allOrderNR.remove(delOrder);
     }
 
+    
+    /** 
+     * Get statuscode.
+     * 
+     * @return String
+     */
     public String getStatuscode() {
         return statuscode;
     }
 
+    
+    /** 
+     * Set statuscode.
+     * 
+     * @param statuscode to be set
+     */
     public void setStatuscode(String statuscode) {
         this.statuscode = statuscode;
     }
 
-   
-
-    public HashSet<Order> getAllOrderNR() {
+    
+    /** 
+     * Get all orders.
+     * 
+     * @return Set<Order>
+     */
+    public Set<Order> getAllOrderNR() {
         return allOrderNR;
     }
 
+    
+    /** 
+     * Set all orders.
+     * 
+     * @param allOrderNR to be set
+     */
     public void setAllOrderNR(HashSet<Order> allOrderNR) {
         this.allOrderNR = allOrderNR;
     }
 
-    public HashSet<OrderDetails> getAllOrderDetailNR() {
+    
+    /** 
+     * Get all oderdetails.
+     * 
+     * @return Set<OrderDetails>
+     */
+    public Set<OrderDetails> getAllOrderDetailNR() {
         return allOrderDetailNR;
     }
 
+    
+    /** 
+     * Set all orderdetails.
+     * 
+     * @param allOrderDetailNR to be set
+     */
     public void setAllOrderDetailNR(HashSet<OrderDetails> allOrderDetailNR) {
         this.allOrderDetailNR = allOrderDetailNR;
     }
 
+    
+    /** 
+     * Add order details.
+     * 
+     * @param details to be added
+     * @return boolean
+     */
     public boolean addOrderDetail(OrderDetails details){
         return allOrderDetailNR.add(details);
     }
 
+    
+    /** 
+     * Remove order details.
+     * 
+     * @param details to be removed
+     * @return boolean
+     */
     public boolean removeOrderDetail(OrderDetails details){
         return allOrderDetailNR.remove(details);
     }
 
   
+ 
+ /** 
+  * Status to string.
+  
+  * @return String
+  */
  @Override
     public String toString() {
         return "Status [allOrderDetailNR=" + allOrderDetailNR + ", allOrderNR=" + allOrderNR + ", statusID=" + statusID
                 + ", statuscode=" + statuscode + ", version=" + version + "]";
     }
 
-//TODO: in allen Entities checken ob wir en Set oder eine Liste brauchen
+//TODO: in allen Entities checken ob wir ein Set oder eine Liste brauchen
 //TODO: in allen Entities add und remove methoden die boolean zurueckgeben
 }
