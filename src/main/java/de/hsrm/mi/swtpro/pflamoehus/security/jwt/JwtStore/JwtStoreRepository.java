9bee1e302b3f5992297302be27783ed620992f9f
Ann-Cathrin Fabian <anncathrin.fabian@gmail.com>
2021-01-31 18:40:12 +0100

    #281 RepoTest

--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/security/jwt/JwtStore/JwtStoreRepository.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/security/jwt/JwtStore/JwtStoreRepository.java
@@ -1,5 +1,6 @@
 package de.hsrm.mi.swtpro.pflamoehus.security.jwt.JwtStore;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface JwtStoreRepository extends JpaRepository<JwtStore, Long>{
 
     boolean existsByToken (String token);
+
+    List<JwtStore> findAll();
 
     Optional<JwtStore> findJwtStoreById(Long id);
 
