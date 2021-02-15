d962825262f695e34816df21fc0deb9c4d8d3757
Ann-Cathrin Fabian <anncathrin.fabian@gmail.com>
2021-02-15 18:49:40 +0100

    Unused Import raus

--- src/main/java/de/hsrm/mi/swtpro/pflamoehus/security/jwt/JwtStore/JwtStoreServiceImpl.java
+++ src/main/java/de/hsrm/mi/swtpro/pflamoehus/security/jwt/JwtStore/JwtStoreServiceImpl.java
@@ -1,7 +1,6 @@
 package de.hsrm.mi.swtpro.pflamoehus.security.jwt.JwtStore;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.persistence.OptimisticLockException;
 import javax.transaction.Transactional;
